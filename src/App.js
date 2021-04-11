041aba2f662f3b3e058a9e5c847965338b4dbd4a
Mamadou Ouologuem <mamadou.ouologuem02@gmail.com>
2021-04-11 22:04:57 +0200

    🎉 Project installation

--- src/App.js
+++ src/App.js
@@ -10,38 +10,37 @@ import Button from './Button';
 const apiToken = '<<Copiez le token de Spotify ici>>';
 
 function shuffleArray(array) {
-  let counter = array.length;
+	let counter = array.length;
 
-  while (counter > 0) {
-    let index = getRandomNumber(counter);
-    counter--;
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
+	while (counter > 0) {
+		let index = getRandomNumber(counter);
+		counter--;
+		let temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
 
-  return array;
+	return array;
 }
 
 /* Return a random number between 0 included and x excluded */
 function getRandomNumber(x) {
-  return Math.floor(Math.random() * x);
+	return Math.floor(Math.random() * x);
 }
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">Bienvenue sur le Blindtest</h1>
-      </header>
-      <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
-      </div>
-      <div className="App-buttons">
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h1 className='App-title'>Bienvenue sur le Blindtest</h1>
+			</header>
+			<div className='App-images'>
+				<p>Il va falloir modifier le code pour faire un vrai Blindtest !!</p>
+			</div>
+			<div className='App-buttons'></div>
+		</div>
+	);
+};
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => (
-  <button onClick={props.onClick}>{props.children}</button>
+	<button onClick={props.onClick}>{props.children}</button>
 );
 
 export default Button;
